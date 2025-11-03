811519cd821d6e2d389db581647c6821797f07d2
Abderahmane BEHAR RAHALA <abehar-r@c1r2s5.marseille.42.school>
2025-11-03 15:47:49 +0100

    project(common-core-libft): Implemented ft_isalpha, ft_isdigit functions | Code ft_isalnum function

---
+++ modules/programming-fundamentals/common-core-libft/main.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abehar-r <abehar-r@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/03 13:57:55 by abehar-r          #+#    #+#             */
+/*   Updated: 2025/11/03 14:58:09 by abehar-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	// #############################################
+	printf("\n");
+	printf(
+        	" _     _  ____  _____ _____ \n"
+        	"/ \\   / \\/  __\\/    //__ __\\\n"
+        	"| |   | || | //|  __\\  / \\  \n"
+        	"| |_/\\| || |_\\\\| |     | |  \n"
+        	"\\____/\\_/\\____/\\_/     \\_/  \n"
+        	"                            \n"
+    	);
+	printf("\n");
+	// #############################################
+
+	// Variables
+	int	isalpha_var = '-';
+	int	isdigit_var = '8';
+
+	// Tests
+	printf("### FUNCTIONS TEST ###\n");
+	printf("\nft_isalpha(%d): %d\n", isalpha_var, ft_isalpha(isalpha_var));
+	printf("ft_isdigit(%d): %d\n", isdigit_var, ft_isdigit(isdigit_var));
+
+	// END
+	printf("\n### END ###\n");
+	return (0);
+}
