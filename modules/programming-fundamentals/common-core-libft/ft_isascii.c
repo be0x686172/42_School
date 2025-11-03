a6fa7728ef51e1e76e3c0edd1b9cb4eab3a9dfed
Abderahmane BEHAR RAHALA <abehar-r@c1r2s5.marseille.42.school>
2025-11-03 16:24:12 +0100

    project(common-core-libft): Implemented ft_isascii function | Code ft_isprint function

--- modules/programming-fundamentals/common-core-libft/ft_isascii.c
+++ modules/programming-fundamentals/common-core-libft/ft_isascii.c
@@ -1,21 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abehar-r <abehar-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/03 13:43:43 by abehar-r          #+#    #+#             */
-/*   Updated: 2025/11/03 16:15:34 by abehar-r         ###   ########.fr       */
+/*   Created: 2025/11/03 16:15:55 by abehar-r          #+#    #+#             */
+/*   Updated: 2025/11/03 16:19:35 by abehar-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-
-#endif
+int	ft_isascii(int c)
+{
+	if (c >= 0 && c <= 127)
+		return (1);
+	return (0);
+}
--- modules/programming-fundamentals/common-core-libft/main.c
+++ modules/programming-fundamentals/common-core-libft/main.c
@@ -6,7 +6,7 @@
 /*   By: abehar-r <abehar-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 13:57:55 by abehar-r          #+#    #+#             */
-/*   Updated: 2025/11/03 16:11:46 by abehar-r         ###   ########.fr       */
+/*   Updated: 2025/11/03 16:23:34 by abehar-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,15 @@ int	main(void)
 	// Variables
 	int	isalpha_var = '-';
 	int	isdigit_var = '8';
-	int	isalnum_var = 'q';
+	int	isalnum_var = 58;
+	int	isascii_var = 127;
 
 	// Tests
 	printf("### FUNCTIONS TESTS ###\n");
 	printf("\nft_isalpha(%d): %d\n", isalpha_var, ft_isalpha(isalpha_var));
 	printf("ft_isdigit(%d): %d\n", isdigit_var, ft_isdigit(isdigit_var));
 	printf("ft_isalnum(%d): %d\n", isalnum_var, ft_isalnum(isalnum_var));
+	printf("ft_isascii(%d): %d\n", isascii_var, ft_isascii(isascii_var));
 
 	// END
 	printf("\n### END ###\n");
