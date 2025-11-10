8bbf5a38b00ee25e7ee66d20088f2bac4682ff4b
be0x686172 <abderahmane.beharrahala@gmail.com>
2025-11-10 16:27:02 +0100

    project(common-core-libft): Implented ft_substr function | Test this function with libftTester

--- modules/programming-fundamentals/common-core-libft/src/ft_substr.c
+++ modules/programming-fundamentals/common-core-libft/src/ft_substr.c
@@ -1,18 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_substr.c                                      :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abehar-r <abehar-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/10 15:12:43 by abehar-r          #+#    #+#             */
-/*   Updated: 2025/11/10 16:24:54 by abehar-r         ###   ########.fr       */
+/*   Created: 2025/11/10 15:46:48 by abehar-r          #+#    #+#             */
+/*   Updated: 2025/11/10 16:26:15 by abehar-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
-void	test_substr(void)
+char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
-	printf("\n - ft_substr(): %s", ft_substr("Hello, World !", 13, 17));
+	char	*ptr;
+
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+	{
+		ptr = malloc(1);
+		if (!ptr)
+			return (NULL);
+		ptr[0] = '\0';
+		return (ptr);
+	}
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	ptr = malloc(len + 1);
+	if (!ptr)
+		return (NULL);
+	ft_memcpy(ptr, s + start, len);
+	ptr[len] = '\0';
+	return (ptr);
 }
