26d8de055e95dec83a4ab6b22be353d22969341c
be0x686172 <abderahmane.beharrahala@gmail.com>
2025-11-11 10:38:25 +0100

    project(common-core-libft): Continue code for ft_strtrim function

--- modules/programming-fundamentals/common-core-libft/src/ft_strtrim.c
+++ modules/programming-fundamentals/common-core-libft/src/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: abehar-r <abehar-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 09:26:07 by abehar-r          #+#    #+#             */
-/*   Updated: 2025/11/11 09:26:35 by abehar-r         ###   ########.fr       */
+/*   Updated: 2025/11/11 10:38:25 by abehar-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	int		i;
+	int		j;
+	size_t	len_s1;
+	char	*ptr;
+
+	len_s1 = ft_strlen(s1);
+	i = 0;
+	while (s1[i])
+	{
+		while (set[j])
+		i++;
+	}
+	return (ptr);
 }
