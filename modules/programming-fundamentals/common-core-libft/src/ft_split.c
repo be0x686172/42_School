5e6fd1ba5d5dd0111aafaab8aaa989b0d376e57e
be0x686172 <abderahmane.beharrahala@gmail.com>
2025-11-14 03:59:28 +0100

    project(common-core-libft): Continue code for ft_split function

--- modules/programming-fundamentals/common-core-libft/src/ft_split.c
+++ modules/programming-fundamentals/common-core-libft/src/ft_split.c
@@ -25,26 +25,59 @@ static int	count_words(const char *s, char c)
 {
 	int		i;
 	int		words;
-	int		in_word;
+	int		in_word_flag;
 
 	i = 0;
 	words = 0;
-	in_word = 0;
+	in_word_flag = 0;
 	while (s[i])
 	{
-		if (s[i] != c && !in_word)
+		if (s[i] != c && !in_word_flag)
 		{
-			in_word = 1;
+			in_word_flag = 1;
 			words++;
 		}
 		else if (s[i] == c)
-			in_word = 0;
+			in_word_flag = 0;
 		i++;
 	}
 	return (words);
 }
 
-void	**ft_split(const char *s, char c)
+static void	count_word_letters(const char *s, char c, char **s_splited)
+{
+	int	i;
+	int	letters;
+	int	j;
+
+	i = 0;
+	letters = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+			letters++;
+		if (s[i] == c && letters > 0)
+		{
+			// printf("\n\t| Nombre de lettres dans le mot: %d", letters);
+			s_splited[j] = malloc(sizeof(char) * letters + 1);
+			j++;
+			letters = 0;
+		}
+		i++;
+	}
+	// printf("\n\t| Nombre de lettres dans le mot: %d et j: %d", letters, j);
+	s_splited[j] = malloc(sizeof(char) * letters + 1);
+}
+
+static void	put_word_letters(const char *s, char c, char **s_splited)
+{
+	int	i;
+
+	i = 0;
+}
+
+void	ft_split(const char *s, char c)
 {
 	char	*s_trimmed;
 	int		words_counted;
@@ -53,5 +86,8 @@ void	**ft_split(const char *s, char c)
 	s_trimmed = trim_s(s, c);
 	words_counted = count_words(s_trimmed, c);
 	s_splited = malloc(sizeof(char *) * (words_counted + 1));
-	printf("\n\tnombre de mots : %d", words_counted);
+	printf("\n\t| Chaine trimé : '%s'", s_trimmed);
+	printf("\n\t| nombre de mots : %d", words_counted);
+	count_word_letters(s_trimmed, c, s_splited);
+	put_word_letters(s, c, s_splited);
 }
